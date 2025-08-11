847a09a5fb3d8633c646658214381a9d9c7f2596
Brian Grinstead <briangrinstead@gmail.com>
2025-08-11 11:12:30 -0700

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/intput-box/" : "/",
+  base: command === "build" ? "/input-box/" : "/",
 }));
